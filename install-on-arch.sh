4edd97e1cc7f67c7b2ebfdfe4fcf56d1ecd00349
Peter Farr <Peter@PrismaPhonic.com>
2021-04-22 10:25:12 -0700

    add spicetify because why not make it spicy

--- install-on-arch.sh
+++ install-on-arch.sh
@@ -84,17 +84,18 @@ case $paru in
 [yY][eE][sS]|[yY])
 	cargo install paru
 
-	paru -S --noconfirm --needed picom-jonaburg-git acpi rofi-git candy-icons-git wmctrl alacritty playerctl brightnessctl dunst xmonad-contrib jq xclip maim rofi-greenclip spotify betterlockscreen
+	paru -S --noconfirm --needed picom-jonaburg-git acpi rofi-git candy-icons-git wmctrl alacritty playerctl brightnessctl dunst xmonad-contrib jq xclip maim rofi-greenclip spotify spicetify-cli spicetify-themes-git betterlockscreen
 	;; 
 
 [nN][oO]|[nN])
 	echo "Installing Other Stuff then"
-	paru -S --noconfirm --needed picom-jonaburg-git acpi rofi-git candy-icons-git wmctrl alacritty playerctl brightnessctl dunst xmonad-contrib jq xclip maim rofi-greenclip spotify betterlockscreen
-	;;
+	paru -S --noconfirm --needed picom-jonaburg-git acpi rofi-git candy-icons-git wmctrl alacritty playerctl brightnessctl dunst xmonad-contrib jq xclip maim rofi-greenclip spotify spicetify-cli spicetify-themes-git betterlockscreen
+	;; 
+
 
 [*])
 	echo "Lets do it anyways lol" 
-	paru -S --noconfirm --needed picom-jonaburg-git acpi rofi-git candy-icons-git wmctrl alacritty playerctl brightnessctl dunst xmonad-contrib jq xclip maim rofi-greenclip spotify betterlockscreen
+	paru -S --noconfirm --needed picom-jonaburg-git acpi rofi-git candy-icons-git wmctrl alacritty playerctl brightnessctl dunst xmonad-contrib jq xclip maim rofi-greenclip spotify spicetify-cli spicetify-themes-git betterlockscreen
 	sleep 1
 	;;
 esac
